38389e92f3d8e696b839e31738286e099c982926
Tim <nebukam@gmail.com>
2025-07-19 15:28:42 -0400

    API Updates

--- Source/PCGExtendedToolkitZoneGraph/Public/Graph/PCGExClusterToZoneGraph.h
+++ Source/PCGExtendedToolkitZoneGraph/Public/Graph/PCGExClusterToZoneGraph.h
@@ -174,7 +174,7 @@ namespace PCGExClusterToZoneGraph
 
 		virtual bool IsTrivial() const override { return false; }
 
-		virtual bool Process(TSharedPtr<PCGExMT::FTaskManager> InAsyncManager) override;
+		virtual bool Process(const TSharedPtr<PCGExMT::FTaskManager>& InAsyncManager) override;
 		bool BuildChains();
 		virtual void CompleteWork() override;
 		void InitComponents();
